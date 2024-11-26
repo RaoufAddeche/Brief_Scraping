0bc7dd525fb084705938cc9a90e8eb68acaf6530
NicolasCassonnet <nicolas.cassonnet@wanadoo.fr>
2024-11-26 11:13:34 +0100

    dans spiders, pip install ipython

--- parquet_scraper/parquet_scraper/spiders/parquetspider.py
+++ parquet_scraper/parquet_scraper/spiders/parquetspider.py
@@ -1,10 +1,12 @@
 import scrapy
 
-
-class ParquetspiderSpider(scrapy.Spider):
+class ParquetSpider(scrapy.Spider):
     name = "parquetspider"
     allowed_domains = ["boutique-parquet.com"]
     start_urls = ["https://boutique-parquet.com"]
 
+    # def start_requests(self):
+    #     super().start_requests()
+                       
     def parse(self, response):
         pass
