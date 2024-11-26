fd1633c28de0b1f8c9856fe4b6c655604e80d8c2
NicolasCassonnet <nicolas.cassonnet@wanadoo.fr>
2024-11-26 10:34:00 +0100

    modify settings.py

--- parquet_scraper/parquet_scraper/settings.py
+++ parquet_scraper/parquet_scraper/settings.py
@@ -44,15 +44,15 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "parquet_scraper.middlewares.ParquetScraperSpiderMiddleware": 543,
-#}
+SPIDER_MIDDLEWARES = {
+   "parquet_scraper.middlewares.ParquetScraperSpiderMiddleware": 543,
+}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "parquet_scraper.middlewares.ParquetScraperDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   "parquet_scraper.middlewares.ParquetScraperDownloaderMiddleware": 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "parquet_scraper.pipelines.ParquetScraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "parquet_scraper.pipelines.ParquetScraperPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
