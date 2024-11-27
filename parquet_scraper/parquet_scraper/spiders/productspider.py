0e49515e5c57d5d223dded582d72fe8e490fddc8
NicolasCassonnet <nicolas.cassonnet@wanadoo.fr>
2024-11-27 14:26:36 +0100

    wednesday after lunch commit

--- parquet_scraper/parquet_scraper/spiders/productspider.py
+++ parquet_scraper/parquet_scraper/spiders/productspider.py
@@ -12,7 +12,15 @@ class ProductSpider(scrapy.Spider):
                 
         for menu_item in child_category_items :
             next_page_url = menu_item.css('a ::attr(href)').get()
-            yield response.follow(next_page_url, callback=self.parse_product)
+            yield response.follow(next_page_url, callback=self.parse_product_list)
+
+    def parse_product_list(self, response):
+        product_grid = response.css("ol.product-grid")
+        products = product_grid.css("a.product-item-photo")
+
+        for product in products :  
+            url_product = product.css("::attr(href)").get()
+            yield response.follow(url_product, callback=self.parse_product)    
 
     def parse_product(self, response):
         product_item = items.ProductItem()
@@ -21,13 +29,10 @@ class ProductSpider(scrapy.Spider):
         name_view = response.xpath("//span[@data-ui-id='page-title-wrapper']")
         product_item['name'] = name_view.css(" ::text").get()
 
-        for truc in response.css("div.product-view"):
-            if truc ==None :
-                return 
-            
-            sku_view = truc.css(".product .attribute .sku")
-            product_item['unique_id'] = sku_view.css("div.value ::text").get()
-    
-            product_item['price'] = truc.css("span.price ::text").get()
-            yield product_item
+        product_view  = response.css("div .product-view")
+        sku_view = product_view.css("div .sku")
+        product_item['unique_id'] = sku_view.css("div.value ::text").get()
+
+        product_item['price'] = product_view.css("span.price ::text").get()
+        yield product_item
 
